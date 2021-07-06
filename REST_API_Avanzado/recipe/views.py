da46f8423e736ba60a8cea050a12e97154e9f7b4
Roo <robert.martinez.delafuente@gmail.com>
2021-07-06 18:07:02 +0200

    Created Recipe viewset

--- REST_API_Avanzado/recipe/views.py
+++ REST_API_Avanzado/recipe/views.py
@@ -31,3 +31,15 @@ class IngredientViewSet(BaseRecipeViewSet):
     """ Manejar los Ingredientes en la base de datos """
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """ Manejar los Ingredientes en la base de datos """
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """ Retonar objetos para el usuario autenticado """
+        return self.queryset.filter(user=self.request.user)
+    
