00ebd5c09ee1ceedb3371f8068831d88bc8858e5
Roo <robert.martinez.delafuente@gmail.com>
2021-07-06 12:30:33 +0200

    Added function to create new tag

--- REST_API_Avanzado/recipe/views.py
+++ REST_API_Avanzado/recipe/views.py
@@ -7,7 +7,7 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """ Manejar los Tags en la BBDD """
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -17,3 +17,7 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """ Filtrar y mostrar por nombre los Tags del usuario autenticado """
         return self.queryset.filter(user=self.request.user).order_by('-name')
+    
+    def perform_create(self, serializer):
+        """ Crear nuevo tag """
+        serializer.save(user=self.request.user)
