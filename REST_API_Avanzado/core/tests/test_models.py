b78f7890eabde5f1f396287a8c9d97d8ddeceeef
Roo <robert.martinez.delafuente@gmail.com>
2021-07-06 10:40:10 +0200

    Added test_tag_str

--- REST_API_Avanzado/core/tests/test_models.py
+++ REST_API_Avanzado/core/tests/test_models.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+def sample_user(email='test@datadosis.com', password='testpass'):
+    """ Crear usuario de Ejemplo """
+    return get_user_model().objects.create_user(email, password)
+
 class ModelTest(TestCase):
 
   def test_create_user_with_email_succesfuly(self):
@@ -38,3 +44,12 @@ class ModelTest(TestCase):
 
     self.assertTrue(user.is_superuser)
     self.assertTrue(user.is_staff)
+  
+  def test_tag_str(self):
+    """ Probar representación en cadena de texto del tag """
+    tag = models.Tag.objects.create(
+      user=sample_user(),
+      name='Meat'
+    )
+
+    self.assertEqual(str(tag), tag.name)
